e3b52b27724bb245cb82991af33837bccf20e200
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-19 19:15:03 +0600

    Setting up project
    Module : 11 , Video : 2

---
+++ app/AppNavigator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const AppNavigator = () => {
+    return (
+        <View>
+        <Text>App Navigator</Text>
+    </View>
+    );
+};
+
+export default AppNavigator;
---
+++ app/screens/DishDetailScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const DishDetailScreen = () => {
+    return (
+        <View>
+            <Text>Dish Detaiil Screen</Text>
+        </View>
+    );
+};
+
+export default DishDetailScreen;
