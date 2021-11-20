331b8aaf7e666dd97b61fa250140dc2dc2c85dbd
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-20 20:26:46 +0600

    Adding Icon to Header
    Video : 12

--- app/AppNavigator.js
+++ app/AppNavigator.js
@@ -1,11 +1,10 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { Text, View } from 'react-native';
 import DishDetailScreen from './screens/DishDetailScreen';
 import HomeScreen from './screens/HomeScreen';
 import MenuScreen from './screens/MenuScreen';
 import {createStackNavigator} from '@react-navigation/stack';
+import Icon from './components/Icon';
 
 
 
@@ -14,17 +13,19 @@ const Stack = createStackNavigator();
 
 const MenuStack = () => {
    return (
-        <Stack.Navigator screenOptions={{
-            headerStyle: {
-                backgroundColor: "#F53B50",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-                fontWeight: 'bold',
-            }
-        }}>
-                <Stack.Screen name="Menu" component={MenuScreen} />
-                <Stack.Screen name="Dish Detail" component={DishDetailScreen} options={({route})=> ({title: route.params.dish.name})} />
+        <Stack.Navigator 
+            screenOptions={{
+                headerRight: () => (<Icon />),
+                headerStyle: {
+                    backgroundColor: "#F53B50",
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                }
+            }}>
+            <Stack.Screen name="Menu" component={MenuScreen} />
+            <Stack.Screen name="Dish Detail" component={DishDetailScreen} options={({route})=> ({title: route.params.dish.name})} />
         </Stack.Navigator>
     )
 }
