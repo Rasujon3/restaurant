bfc16c41c0c66489d3950123ca8d2e3048fe5806
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-20 21:18:01 +0600

    Designing Item Card
    Video : 16

--- app/screens/favouriteScreen.js
+++ app/screens/favouriteScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import Card from '../components/Card';
 
 const mapStateToProps = state => {
     return {
@@ -13,7 +14,7 @@ const favouriteScreen = (props) => {
         <View>
             <FlatList 
                 data={props.favourites}
-                renderItem={({item})=> ( <Text>{item.name}</Text> )}
+                renderItem={({item})=> ( <Card item={item} /> )}
                 keyExtractor={item => item.id.toString()}
             />
         </View>
